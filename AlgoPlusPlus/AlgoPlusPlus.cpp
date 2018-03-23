a303a8549fdcf6bc5fc9c05d1e10ce732347cd57
Sameeh Jubran <sameeh.j@gmail.com>
2018-03-23 17:41:58 +0200

    Graph: Fix BFS
    Signed-off-by: Sameeh Jubran <sameeh.j@gmail.com>

--- AlgoPlusPlus/AlgoPlusPlus.cpp
+++ AlgoPlusPlus/AlgoPlusPlus.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include "Graph.h"
+
 using namespace std;
+
+void ExampleGraphBFS()
+{
+	Graph g(10);
+	g.AddEdge(1, 2);
+	g.AddEdge(1, 3);
+	g.AddEdge(1, 5);
+
+	g.AddEdge(2, 4);
+	g.AddEdge(2, 5);
+	
+	g.AddEdge(3, 5);
+	g.AddEdge(3, 6);
+
+	g.AddEdge(4, 5);
+	g.AddEdge(4, 8);
+
+	g.AddEdge(5, 6);
+
+	g.AddEdge(6, 7);
+	g.AddEdge(6, 9);
+
+	g.AddEdge(7, 4);
+	g.AddEdge(7, 5);
+	g.AddEdge(7, 8);
+
+	g.AddEdge(9, 7);
+	g.AddEdge(9, 8);
+
+	g.BFS(1);
+}
 
 int main()
 {
 	cout << "Hello World!" << endl;
+	ExampleGraphBFS();
 	return 0;
 }
--- AlgoPlusPlus/Graph.cpp
+++ AlgoPlusPlus/Graph.cpp
@@ -16,6 +16,10 @@ Graph::Graph(int nodes) : m_Size(nodes)
 	for (int i = 0; i < nodes; ++i)
 	{
 		m_Map[i] = new bool[nodes];
+		for (int j = 0; j < m_Size; j++)
+		{
+			m_Map[i][j] = false;
+		}
 	}
 }
 
@@ -46,12 +50,41 @@ bool Graph::AddEdge(int u, int v)
 	return true;
 }
 
-void print_queue(queue<int> q)
+void print_queue(queue<int> q, int start = 0, int end = 1000)
 {
+	int i = 0;
+	while (!q.empty() && i < end)
+	{
+		if (i >= start)
+		{
+			std::cout << q.front() << " ";
+		}
+		q.pop();
+		i++;
+	}
+	std::cout << std::endl;
+}
+
+void print_queue_layers(queue<int> q)
+{
+	int i = 0;
+	int j = 0;
+
 	while (!q.empty())
 	{
-		std::cout << q.front() << " ";
+		if (q.front() == -1)
+		{
+			q.pop();
+			if (q.empty())
+			{
+				break;
+			}
+			cout << endl << " LeveL " << j++ << ":" << endl;
+		}
+
+		std::cout <<q.front() << " ";
 		q.pop();
+		i++;
 	}
 	std::cout << std::endl;
 }
@@ -59,28 +92,52 @@ void print_queue(queue<int> q)
 void Graph::BFS(int starting_node)
 {
 	queue<int> q;
-	int j,i;
+	queue<int> layers_q;
+	int i;
 	int current_node;
+	bool *visited;
+	int current_layer_count = 0;
+	int last_layer_count = 0;
 
 	if (starting_node <= 0 || starting_node > m_Size)
 	{
 		throw std::exception("Illegal value for starting node");
 	}
+	visited = new bool[m_Size];
+	for (i = 0; i < m_Size; i++)
+	{
+		visited[i] = false;
+	}
 
 	q.push(starting_node);
-	j = 0;
+	layers_q.push(-1);
+	layers_q.push(starting_node);
+	layers_q.push(-1);
+	last_layer_count = 1;
+	current_layer_count = 0;
+	visited[starting_node] = true;
+
 	do {
-		cout << "layer " << j << ":" << endl;
-		print_queue(q);
-		j++;
 		current_node = q.front();
 		q.pop();
 		for (i = 0; i < m_Size; i++)
 		{
-			if (m_Map[current_node][i])
+			if (m_Map[current_node][i] && !visited[i])
 			{
 				q.push(i);
+				current_layer_count++;
+				layers_q.push(i);
+				visited[i] = true;
 			}
 		}
+
+		last_layer_count--;
+
+		if (last_layer_count == 0)
+		{
+			last_layer_count = current_layer_count;
+			layers_q.push(-1);
+		}
 	} while (!q.empty());
+	print_queue_layers(layers_q);
 }
