877b366afcfece7c183570ecb2fb7ffc22407571
Sameeh Jubran <sameeh.j@gmail.com>
2018-03-23 13:19:13 +0200

    Implement BFS
    Signed-off-by: Sameeh Jubran <sameeh.j@gmail.com>

--- AlgoPlusPlus/Graph.cpp
+++ AlgoPlusPlus/Graph.cpp
@@ -1,5 +1,8 @@
 #include "Graph.h"
 #include <stdexcept>
+#include <queue>
+
+using namespace std;
 
 
 Graph::Graph(int nodes) : m_Size(nodes)
@@ -23,5 +26,61 @@ Graph::~Graph()
 		delete m_Map[i];
 	}
 	delete m_Map;
+}
 
+bool Graph::AddEdge(int u, int v)
+{
+	if (v <= 0 || v > m_Size)
+	{
+		throw std::exception("Illegal value for starting node");
+	}
+	if (u <= 0 || u > m_Size)
+	{
+		throw std::exception("Illegal value for starting node");
+	}
+	if (m_Map[u][v])
+	{
+		return false;
+	}
+	m_Map[u][v] = true;
+	return true;
+}
+
+void print_queue(queue<int> q)
+{
+	while (!q.empty())
+	{
+		std::cout << q.front() << " ";
+		q.pop();
+	}
+	std::cout << std::endl;
+}
+
+void Graph::BFS(int starting_node)
+{
+	queue<int> q;
+	int j,i;
+	int current_node;
+
+	if (starting_node <= 0 || starting_node > m_Size)
+	{
+		throw std::exception("Illegal value for starting node");
+	}
+
+	q.push(starting_node);
+	j = 0;
+	do {
+		cout << "layer " << j << ":" << endl;
+		print_queue(q);
+		j++;
+		current_node = q.front();
+		q.pop();
+		for (i = 0; i < m_Size; i++)
+		{
+			if (m_Map[current_node][i])
+			{
+				q.push(i);
+			}
+		}
+	} while (!q.empty());
 }
--- AlgoPlusPlus/Graph.h
+++ AlgoPlusPlus/Graph.h
@@ -1,9 +1,12 @@
 #pragma once
+#include <iostream>
 
 class Graph {
 public:
 	Graph(int nodes);
 	~Graph();
+	bool AddEdge(int u, int v);
+	void BFS(int starting_node);
 
 private:
 	bool **m_Map;
