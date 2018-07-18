162ce4aa5fa713eee4469c505e762c8ec8410647
sameeh <sameeh@daynix.com>
2018-07-19 00:42:06 +0300

    Reimplement the graph with a proper iterator class
    Signed-off-by: sameeh <sameeh@daynix.com>

--- AlgoPlusPlus/AlgoPlusPlus.cpp
+++ AlgoPlusPlus/AlgoPlusPlus.cpp
@@ -3,42 +3,25 @@
 
 using namespace std;
 
-void ExampleGraphBFS()
-{
-	Graph g(10);
-	g.AddEdge(1, 2);
-	g.AddEdge(1, 3);
-	g.AddEdge(1, 5);
-
-	g.AddEdge(2, 4);
-	g.AddEdge(2, 5);
-	
-	g.AddEdge(3, 5);
-	g.AddEdge(3, 6);
-
-	g.AddEdge(4, 5);
-	g.AddEdge(4, 8);
-
-	g.AddEdge(5, 6);
-
-	g.AddEdge(6, 7);
-	g.AddEdge(6, 9);
-
-	g.AddEdge(7, 4);
-	g.AddEdge(7, 5);
-	g.AddEdge(7, 8);
-
-	g.AddEdge(9, 7);
-	g.AddEdge(9, 8);
-
-	g.BFS(1);
-	g.DFS(1);
-
-}
-
 int main()
 {
 	cout << "Hello World!" << endl;
-	ExampleGraphBFS();
+	set<int> nodes = { 1,2,3,4,5,6,7,8,9 };
+
+	Graph<int> graph (nodes);
+	graph.AddEdge(1, 2);
+	graph.AddEdge(1, 2);
+	graph.AddEdge(2, 4);
+	graph.AddEdge(7, 2);
+	graph.AddEdge(3, 6);
+	graph.AddEdge(6, 2);
+
+	Graph<int>::BFSiterator it = graph.begin(2);
+	while (it.hasNext())
+	{
+		cout << it.getCurrent() << endl;
+		it.next();
+	}
+
 	return 0;
 }
--- AlgoPlusPlus/Graph.h
+++ AlgoPlusPlus/Graph.h
@@ -1,21 +1,89 @@
 #pragma once
 #include <iostream>
 #include <set>
+#include <list>
+#include <map>
+#include <memory>
+#include <queue>
 
 using namespace std;
 
+template<typename T>
 class Graph {
 public:
-	Graph(int nodes);
-	~Graph();
-	bool AddEdge(int u, int v);
-	void BFS(int starting_node);
-	void DFS(int starting_node);
+	Graph(set<T> nodes) : m_Map()
+	{
+		for (auto node : nodes)
+		{
+			set<T> vec;
+			m_Map.insert(pair<T, set<T>> (node, vec));
+		}
+	}
 
+	~Graph() {}
+
+	void AddEdge(T u, T v)
+	{
+		map<T, set<T>>::iterator it;
+		it = m_Map.find(u);
+		it->second.insert(v);
+		it = m_Map.find(v);
+		it->second.insert(u);
+	}
+
+	class BFSiterator
+	{
+	public:
+		BFSiterator(const map<T, set<T>> &mapa, T node) :
+			m_CurrentMap(mapa)
+		{
+			map<T, set<T>>::iterator it = m_CurrentMap.find(node);
+			m_Queue.push(it->first);
+		}
+		BFSiterator(const BFSiterator& other) :
+			m_CurrentMap(other.m_CurrentMap),
+			m_Queue(other.m_Queue),
+			m_Visited(other.m_Visited)
+
+		{ }
+
+		T getCurrent()
+		{
+			return m_Queue.front();
+		}
+		bool hasNext()
+		{
+			return !m_Queue.empty();
+		}
+		BFSiterator& next()
+		{
+			 // pop the element we should visit
+			T current = m_Queue.front();
+			m_Visited.insert(current);
+			m_Queue.pop();
+
+			map<T, set<T>>::iterator it = m_CurrentMap.find(current);
+			for (auto neighbor : it->second)
+			{
+				if (m_Visited.find(neighbor) == m_Visited.end())
+				{
+					m_Queue.push(neighbor);
+					m_Visited.insert(neighbor);
+				}
+			}
+			return *this;
+		}
+	private:
+		map<T, set<T>> m_CurrentMap;
+		queue<T> m_Queue;
+		set<T> m_Visited;
+	};
+
+	BFSiterator begin(T node)
+	{
+		return BFSiterator(m_Map, node);
+	}
 
 private:
-	void DFSRec(set<int>* s, int current_node);
-
-	bool **m_Map;
-	int m_Size;
+	map<T, set<T>> m_Map;
 };
